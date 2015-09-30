582518f481249db7aa140438b8d8047509a73962
Justin Reyes <justin.regi.reyes@gmail.com>
2015-09-30 12:55:47 -0700

    able to grab log.address in gmap.js

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -5,23 +5,40 @@ $(document).ready(function() {
                     map: map,
                     title: 'WORK'
                 });
+  	logAddress();
+  	geolocations();
+
   }
 )
 
 var coordinates = {lat: 37.78, lng: -122.44}
 var map;
 var marker;
-var address = "Los Francisco California".split(" ").join("+");
-$.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + 
-	address + "&key=AIzaSyAeBs3vx7LfuQg8Mj8tEqZ5WvgcwT4DElQ"
-).done(function(data) {
-	console.log(data.results[0].geometry.location)
+var address;
+
+function logAddress() {
+	var add = $('#logAddress div');
+	console.log(add.attr("id"))
+	address = add.attr("id").split(" ").join("+");
 }
-)
-	function initMap() {
+
+function geolocations() {
+	$.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + 
+	address + "&key=AIzaSyAeBs3vx7LfuQg8Mj8tEqZ5WvgcwT4DElQ"
+		).done(function(data) {
+			if (data.results[0]) {
+				console.log(data.results[0].geometry.location)
+			} else {
+				console.log("No Address")
+			}
+		})		
+}
+
+function initMap() {
   		map = new google.maps.Map(document.getElementById('map-canvas'), {
     	center: coordinates,
     	zoom: 8
   		});
-  	}
+}
+
 
