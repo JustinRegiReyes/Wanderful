3dd73eae90ea113c6cf1b1542a7fdcb6674d22a4
Justin Reyes <justin.regi.reyes@gmail.com>
2015-10-01 02:07:21 -0700

    pins content are rendered more consistently

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   	if ($("div#map-canvas").length) {
+      logInfo();
   		initMap();
-  		logInfo();
   	}
 })
 
@@ -9,38 +9,33 @@ var city;
 var map;
 var address;
 var addressArr = [];
-var geolocations;
+var coordinates;
+var coordinatesArr = [];
 
 function logInfo() {
 	var addresses = $('div.panel-body span');
 	for(i = 0; i < addresses.length; i++) {
 		addressArr.push(addresses[i].id)
-	}
+	}   
 }
 
 
-
-
-function bindInfoWindow(marker, map, infowindow, description) {
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(description);
-        infowindow.open(map, marker);
-    });
-}
-
-function bindInfoBubble(marker, map, infobubble, description) {
-    // google.maps.event.addListener(marker, 'click', function() {
-    //     infowindow.setContent(description);
-    //     infowindow.open(map, marker);
-    // });
-      
-      marker.addListener('mouseout', function() {
-        infobubble.close(map, marker);
-      });
-}
 
 function initMap() {
-    var infowindow =  new google.maps.InfoWindow();
+    var infoBubble = new InfoBubble({
+                  shadowStyle: 1,
+                  padding: 1,
+                  backgroundColor: '#FFFFFF',
+                  borderRadius: 0,
+                  arrowSize: 10,
+                  borderWidth: 1,
+                  borderColor: '#FFFFFF',
+                  disableAutoPan: true,
+                  hideCloseButton: true,
+                  arrowPosition: 50,
+                  backgroundClassName: 'transparent',
+                  arrowStyle: 2
+                });
 
 		city = $("header h1").attr("id");
 		$.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + 
@@ -54,58 +49,79 @@ function initMap() {
     		zoom: 12
   			});
 
-        // ====================== Markers ========================== 
-        function markers(coordinates, test) {
-            // for(i = 0; i < addressArr.length; i++) {
-            //   var logTitle = $('div.panel-body p')[i].id
-            //   var logContent = $('#' + logTitle).html()
+        // ====================== Markers ==========================
+        var markersCounter = 0;
+        var logTitleArr = [];
+        var logContentArr = []; 
+        function markers(coordinates) {
+          
+          
+           
+              var logTitle = $('div.panel-body p')[markersCounter].id;
+              logTitleArr.push(logTitle);
+              logContentArr.push($('#' + logTitle).html());
+
+              // console.log(coordinates);
+              // console.log(logTitle);
               
               var marker = new google.maps.Marker({
                               position: new google.maps.LatLng(coordinates.lat, coordinates.lng),
                               map: map,
-                              title: 'WORK'
+                              title: logTitleArr[markersCounter],
+                              content: logContentArr[markersCounter]
                           });
-              google.maps.event.addListener(marker, 'click', function() {
-                infowindow.setContent(test + 'test');
-                infowindow.open(map, marker);
+            // console.log(logTitleArr);
+            // console.log(logContentArr[markersCounter]);
+            // console.log(markersCounter);
+              google.maps.event.addListener(marker, 'mouseover', function() {
+                console.log(this.content);
+                infoBubble.setContent((this.content).toString());
+                infoBubble.open(map, this);
               });
+              google.maps.event.addListener(marker, 'mouseout', function() {
+                infoBubble.setContent((this.content).toString());
+                infoBubble.close(map, this);
+              });
+              markersCounter++;
+              // google.maps.event.addListener(marker,'click', (function(marker,test,infowindow){ 
+              //   return function() {
+              //   infowindow.setContent(test);
+              //   infowindow.open(map,marker);
+              //   };
+              // })(marker,test,infowindow)); 
+    
 
-               // var infoBubble = new InfoBubble({
-               //    shadowStyle: 1,
-               //    padding: 1,
-               //    backgroundColor: '#FFFFFF',
-               //    borderRadius: 0,
-               //    arrowSize: 10,
-               //    borderWidth: 1,
-               //    borderColor: '#FFFFFF',
-               //    disableAutoPan: true,
-               //    hideCloseButton: true,
-               //    arrowPosition: 50,
-               //    backgroundClassName: 'transparent',
-               //    arrowStyle: 2
-               //  });
+               
                 
                //  bindInfoBubble(marker, map, infoBubble, i + 'test');
                // bindInfoWindow(marker, map, infowindow, i +'TEST');
             // }
-          }
+        }
 
-          geolocations = function() {
-              for(i = 0; i < addressArr.length; i++ ) {
-                $.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + 
-                   addressArr[i] + "&key=AIzaSyAeBs3vx7LfuQg8Mj8tEqZ5WvgcwT4DElQ"
+       
+      var counter = 0;
+      function geolocations() {       
+              
+                  $.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + 
+                  addressArr[counter] + "&key=AIzaSyAeBs3vx7LfuQg8Mj8tEqZ5WvgcwT4DElQ"
                 ).done(function(data) {
-                    if (data.results[0]) {
-                    var coordinates = data.results[0].geometry.location;
-                     markers(coordinates, i)
-                    } else {
-                        console.log("No Address")
-                    }
-                })  
-              } 
-          }
+                  if (data.results[0]) {
+                  coordinate = data.results[0].geometry.location;
+                  console.log(addressArr[counter]);
+                  markers(coordinate);
+                  counter++;
+                  }
+                  if(counter < addressArr.length) {
+                    geolocations();
+        
+                  }
+                })
+
+      }
           geolocations();
+
   		})
+    
 
 }
 
